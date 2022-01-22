7d01531f96bcb170b6fe4498077ef3702c23ef27
王珠峰 <734765725@qq.com>
2022-01-23 01:56:49 +0800

    成功设置加密更新token引用包

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module server-by-chat
 
 go 1.17
 
-require github.com/dgrijalva/jwt-go v3.2.0+incompatible
+require github.com/golang-jwt/jwt/v4 v4.2.0
--- main/main.go
+++ main/main.go
@@ -5,9 +5,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
-	"github.com/dgrijalva/jwt-go"
+	"github.com/golang-jwt/jwt/v4"
 )
+
+// 创建一个jwt使用的密钥
+var jwtKey = []byte("my_react_token_key")
 
 // 临时数据库
 var usersDb = map[string]string{
@@ -15,15 +19,14 @@ var usersDb = map[string]string{
 	"456": "qwe",
 }
 
-type LoginApiPramsStrust struct {
+type LoginRequestStrust struct {
 	UserID  string `json:"userID"`
 	UserPwd string `json:"userPwd"`
 }
 
 //token转化结构体
-type ClaimsStruct struct {
-	UserID  string
-	UserPwd string
+type JwtTokenResponseClaimsStruct struct {
+	UserID string
 	jwt.StandardClaims
 }
 
@@ -43,51 +46,55 @@ func loginRouter(res http.ResponseWriter, req *http.Request) {
 	fmt.Println("登陆接口")
 
 	// 响应解码
-	var loginApiPramsStrust LoginApiPramsStrust
-	if err := json.NewDecoder(req.Body).Decode(&loginApiPramsStrust); err != nil {
+	var loginRequestStrust LoginRequestStrust
+	if err := json.NewDecoder(req.Body).Decode(&loginRequestStrust); err != nil {
 		fmt.Println("登陆接口入参对象结构解析失败", err)
 		res.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	fmt.Printf("登陆接口入参对象结构%v\n", loginApiPramsStrust)
-
-	claimsStruct := ClaimsStruct{
-		UserID:  loginApiPramsStrust.UserID,
-		UserPwd: loginApiPramsStrust.UserPwd,
-	}
+	fmt.Printf("登陆接口入参对象结构%v\n", loginRequestStrust)
 
 	// 获取密码
-	expectedPassword, ok := usersDb[claimsStruct.UserID]
+	expectedPassword, ok := usersDb[loginRequestStrust.UserID]
 	if !ok {
 		fmt.Println("没有此用户")
 		res.WriteHeader(http.StatusUnauthorized)
 		return
 	}
-	if claimsStruct.UserPwd != expectedPassword {
+	if loginRequestStrust.UserPwd != expectedPassword {
 		fmt.Println("密码不正确")
 		res.WriteHeader(http.StatusUnauthorized)
 		return
 	}
 	fmt.Println("密码正确")
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claimsStruct)
-	tokenString, err := token.SigningString()
+
+	expirationTime := time.Now().Add(5 * time.Minute)
+	jwtTokenResponseClaimsStruct := &JwtTokenResponseClaimsStruct{
+		UserID: loginRequestStrust.UserID,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwtTokenResponseClaimsStruct)
+	tokenString, err := token.SignedString(jwtKey)
 	if err != nil {
 		fmt.Println("token创建出错", err)
 		res.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	fmt.Println("token创建成功", tokenString)
-	// http.SetCookie(res, &http.Cookie{
-	// 	Name:    "token",
-	// 	Value:   tokenString,
-	// 	Expires: time.Now().Add(5 * time.Minute),
-	// })
-	_, err = res.Write([]byte(tokenString))
-	if err != nil {
-		fmt.Println("token返回客户端失败", err)
-		res.WriteHeader(http.StatusInternalServerError)
-	}
+	http.SetCookie(res, &http.Cookie{
+		Name:    "reactToken",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
+	// _, err = res.Write([]byte(tokenString))
+	// if err != nil {
+	// 	fmt.Println("token返回客户端失败", err)
+	// 	res.WriteHeader(http.StatusInternalServerError)
+	// }
 }
 
 func chatMessageRouter(res http.ResponseWriter, req *http.Request) {
