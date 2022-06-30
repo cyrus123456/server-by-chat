53e323ec98e34e3766f70f4c9e316748fdf0b214
王珠峰 <734765725@qq.com>
2022-06-30 18:07:27 +0800

    操作redis

--- go.mod
+++ go.mod
@@ -5,21 +5,15 @@ go 1.18
 require (
 	github.com/golang-jwt/jwt/v4 v4.4.1
 	github.com/gorilla/websocket v1.5.0
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.10.3
+	google.golang.org/genproto v0.0.0-20220617124728-180714bec0ad
 	google.golang.org/grpc v1.47.0
 	google.golang.org/protobuf v1.28.0
 )
 
 require (
-	github.com/ghodss/yaml v1.0.0 // indirect
-	github.com/golang/glog v1.0.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.10.3 // indirect
 	golang.org/x/net v0.0.0-20220615171555-694bf12d69de // indirect
 	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20220617124728-180714bec0ad // indirect
-	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main/main.go
+++ main/main.go
@@ -14,22 +14,24 @@ import (
 
 func main() {
 
-	// 连接grpc用户消息ID消息生成器服务器
-	conn, err := grpc.Dial("127.0.0.1:8028", grpc.WithInsecure())
-	if err != nil {
-		log.Println("grpc  8028连接服务端失败: \n\r", err)
-		return
-	} else {
-		log.Println("grpc  8028连接服务端成功:\n\r")
-	}
-	defer conn.Close()
-	getMessageIdServiceClient := getMessageIGrpcGateway.NewGetMessageIdServiceClient(conn)
-	getMessageResponse, err := getMessageIdServiceClient.GetMessageId(context.Background(), &getMessageIGrpcGateway.GetMessageRequest{UserId: "123"})
-	if err != nil {
-		log.Println("Grpc GetMessageId调用失败: \n\r", err)
-	} else {
-		log.Println("Grpc GetMessageId调用成功 \n\r", getMessageResponse)
-	}
+	go func() {
+		// 协程连接grpc用户消息ID消息生成器服务器
+		conn, err := grpc.Dial("127.0.0.1:8028", grpc.WithInsecure())
+		if err != nil {
+			log.Println("grpc  8028连接服务端失败: \n\r", err)
+			return
+		} else {
+			log.Println("grpc  8028连接服务端成功:\n\r")
+		}
+		defer conn.Close()
+		getMessageIdServiceClient := getMessageIGrpcGateway.NewGetMessageIdServiceClient(conn)
+		getMessageResponse, err := getMessageIdServiceClient.GetMessageId(context.Background(), &getMessageIGrpcGateway.GetMessageRequest{UserId: "123"})
+		if err != nil {
+			log.Println("Grpc GetMessageId调用失败: \n\r", err)
+		} else {
+			log.Println("Grpc GetMessageId调用成功 \n\r", getMessageResponse)
+		}
+	}()
 
 	http.HandleFunc("/socket", routes.SocketHandler)
 
